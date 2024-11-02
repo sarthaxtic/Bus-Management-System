a7c7baebe7ab60475640708ecb035ba4f57e5b75
Aman Bhatt <amanbhatt0910@gmail.com>
2024-11-02 23:43:16 +0530

    project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    // Just an example
+    <div className="bg-gray-600 h-[100vh] p-10">
+      <h1 className="text-white text-3xl font-bold">Bus-Management-System</h1>
+      <p className="text-gray-100">GEHU, Bhimtal</p>
+    </div>
+  )
+}
+
+export default App
